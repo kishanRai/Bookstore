b1ee495894d4c4189981c0f919a58cccdef7910f
kishan <erkishanrai@gmail.com>
2026-09-12 07:25:21 +0530

    Enhanced Testing framework with Mockito tests for BookController and Docker configuration for local PostgreSQL database to work as standalone application

--- src/test/java/org/example/bookstore/BookControllerTest.java
+++ src/test/java/org/example/bookstore/BookControllerTest.java
@@ -1,13 +1,17 @@
 package org.example.bookstore;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
 import org.example.bookstore.controllers.catalog.BookController;
+import org.example.bookstore.services.catalog.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest( BookController.class )
@@ -16,10 +20,14 @@ public class BookControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	@MockitoBean
+	private BookService bookService;
+
 	@Test
 	public void testGetBooks_whenCatalogIsEmpty() throws Exception {
 		// Implement test logic for GET /books endpoint
 
+		given( bookService.getBooks() ).willReturn( List.of() );
 		mockMvc.perform( get( "/api/v1/books" ).accept( MediaType.APPLICATION_JSON ) )
 			.andExpect( status().isOk() )
 			.andExpect( content().contentType( MediaType.APPLICATION_JSON ) )
